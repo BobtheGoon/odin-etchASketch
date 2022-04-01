4a9ea2e18efdbd52dfc9b9829b5e897888ada7e9
Teemu <teemu9598@gmail.com>
2022-04-01 06:12:56 +0300

    Finish createGrid

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-function createGrid(y, x) {
+function createGrid(size) {
     const container = document.getElementById('container');
-    for (let i = 0; i < y; ++i) {
-        div = document.createElement('div');
-        div.classList.add('pixel');
-        container.appendChild(div);
 
+    for (let i = 0; i < size; ++i) {
+        outerDiv = document.createElement('div');
+        outerDiv.classList.add('vertical');
+        container.appendChild(outerDiv)
+
+        for (let j = 0; j < size; ++j) {
+            innerDiv = document.createElement('div');
+            innerDiv.classList.add('horizontal');
+            outerDiv.appendChild(innerDiv);
+        }
     }
+
 }
 
-createGrid(16, 16)
+createGrid(16)
