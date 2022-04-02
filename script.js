0233bae7acc388126a3b07f35daef4c0f5f93658
Teemu <teemu9598@gmail.com>
2022-04-02 21:19:10 +0300

    Add prompt for new grid size and refresh screen with clear screen

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 function createGrid(size) {
     const container = document.getElementById('container');
 
-    for (let i = 0; i < size; ++i) {
-        outerDiv = document.createElement('div');
-        outerDiv.classList.add('vertical');
-        container.appendChild(outerDiv)
-
-        for (let j = 0; j < size; ++j) {
-            innerDiv = document.createElement('div');
-            innerDiv.classList.add('horizontal');
-            outerDiv.appendChild(innerDiv);
+    if (container.hasChildNodes()) {
+        while(container.hasChildNodes()) {
+            container.removeChild(container.firstChild);
         }
     }
 
+    if (!container.hasChildNodes()) {
+        for (let i = 0; i < size; ++i) {
+            outerDiv = document.createElement('div');
+            outerDiv.classList.add('vertical');
+            container.appendChild(outerDiv)
+
+            for (let j = 0; j < size; ++j) {
+                innerDiv = document.createElement('div');
+                innerDiv.classList.add('horizontal');
+                outerDiv.appendChild(innerDiv);
+            }
+        }
+    }
+    addGridListener()
 }
 
 
@@ -22,19 +30,28 @@ function clearGrid() {
     for (coloreDiv of divs) {
         coloreDiv.style.backgroundColor = 'white'
     }
+
+    promptNewGrid()
+}
+
+
+function promptNewGrid() {
+    let gridSize = prompt('Input grid size')
+    createGrid(gridSize)
+}
+
+
+function addGridListener() {
+    const div = document.getElementsByClassName('horizontal');
+
+    for (i = 0; i<div.length; ++i) {
+    div[i].addEventListener('mouseover', function(event) {
+        event.target.style.backgroundColor = 'black';
+    })}
 }
 
 
 createGrid(16)
-
-
-const div = document.getElementsByClassName('horizontal');
-
-for (i = 0; i<div.length; ++i) {
-div[i].addEventListener('mouseover', function(event) {
-    event.target.style.backgroundColor = 'black';
-})}
-
 
 const btn = document.getElementById('clear');
 btn.addEventListener('click', clearGrid)
